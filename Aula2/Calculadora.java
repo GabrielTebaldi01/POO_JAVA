b1e702604abe6d36b56ecacf72aa1a2339c882b5
GabrielTebaldi01 <gabriel.g.tebaldi@gmail.com>
2024-03-16 11:13:33 -0300

    JAva2

---
+++ Aula2/Calculadora.java
@@ -0,0 +1,24 @@
+package Aula2;
+
+public class Calculadora {
+
+    double numA;
+    double numB;
+
+    double somar(){
+        return numA + numB;
+    }
+
+    double subtrair(){
+        return numA - numB;
+    }
+
+    double dividir(){
+        return numA/numB;
+    }
+
+    double multiplicar(){
+        return numA*numB;
+    }
+    
+}
---
+++ Aula2/Main.java
@@ -0,0 +1,18 @@
+package Aula2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculadora calc= new Calculadora();
+        calc.numA = 2.0;
+        calc.numB = 3.0;
+
+        System.out.println("Soma:" + calc.somar());
+        System.out.println("Subtração:" + calc.subtrair());
+        System.out.println("Divisão:" + calc.dividir());
+        System.out.println("Multiplicação:" + calc.multiplicar());
+    }
+
+    
+    
+}
